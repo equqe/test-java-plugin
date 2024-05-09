b0408b275db028a63966156ab8e9da058f032079
neumanncc <145790372+equqe@users.noreply.github.com>
2024-05-09 20:27:41 +0300

    Update CommandHandler.java

--- src/main/java/com/equqe/auth/CommandHandler.java
+++ src/main/java/com/equqe/auth/CommandHandler.java
@@ -1,6 +1,7 @@
 package com.equqe.auth;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.bukkit.command.Command;
@@ -33,6 +34,29 @@ public class CommandHandler implements CommandExecutor{
                 e.printStackTrace();
             }
 
+            return true;
+        } else if(command.getName().equalsIgnoreCase("login") && sender instanceof Player){
+            Player player = (Player) sender;
+            String password = args[0];
+
+            player.sendMessage("&aTrying to login into your account...");
+
+            // hashing the password to sha256
+            password = org.apache.commons.codec.digest.DigestUtils.sha256Hex(password);
+
+            String checkLoginSQL = "SELECT * FROM player WHERE uid = ? AND password = ?";
+            try {
+                ResultSet result = sqlHelper.query(checkLoginSQL, player.getUniqueId().toString(), password);
+                if (result.next()) {
+                    player.sendMessage("&dSuccessfully logged in!");
+                } else {
+                    player.sendMessage("&cIncorrect password or account not found!");
+                }
+            } catch (SQLException e) {
+                player.sendMessage("&dFailed to login! Something is wrong with the database");
+                e.printStackTrace();
+            } 
+            
             return true;
         }
         return false;
